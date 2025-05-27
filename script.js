c40346fc6eb79139cb80fb9b02977911651879b2
0127mb <muhammedjamolidinov@gmail.com>
2025-05-27 14:49:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let a = document.querySelector('.li');  
+let b = document.querySelector('.lii');  
+let d = document.querySelector('.liii');  
+
+let c = prompt('Your name');  
+let e = prompt('Your surname');  
+let y = +prompt('Your age');  
+
+a.textContent = "Name: " + c;  
+b.textContent = "Surname: " + e;  
+d.textContent = "Age: " + y;  
+
+a.style.color = "black";  
+b.style.color = "black";  
+d.style.color = "black";
